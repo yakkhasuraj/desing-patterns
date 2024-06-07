bc66a6460d40985cb5b38bbb2242ad4cf39b9981
Suraj Yakkha <yakkhasuraj@gmail.com>
2024-06-07 21:12:09 +0545

    feat: implement decorator pattern

---
+++ structural-patterns/decorator.js
@@ -0,0 +1,68 @@
+class Shape {}
+
+class Circle extends Shape {
+  constructor(radius = 0) {
+    super();
+
+    this.radius = radius;
+  }
+
+  resize(factor) {
+    this.radius *= factor;
+  }
+
+  toString() {
+    return `A circle of radius ${this.radius}`;
+  }
+}
+
+class Square extends Shape {
+  constructor(side = 0) {
+    super();
+
+    this.side = side;
+  }
+
+  toString() {
+    return `A square with side ${this.side}`;
+  }
+}
+
+class ColoredShape extends Shape {
+  constructor(shape, color) {
+    super();
+
+    this.shape = shape;
+    this.color = color;
+  }
+
+  toString() {
+    return `${this.shape.toString()} ` + `has the color ${this.color}`;
+  }
+}
+
+class TransparentShape extends Shape {
+  constructor(shape, transparency) {
+    super();
+
+    this.shape = shape;
+    this.transparency = transparency;
+  }
+
+  toString() {
+    return (
+      `${this.shape.toString()} ` +
+      `has ${this.transparency * 100.0}% transparency`
+    );
+  }
+}
+
+let circle = new Circle(2);
+console.log(circle.toString());
+
+let redCircle = new ColoredShape(circle, "red");
+redCircle.shape.resize(2);
+console.log(redCircle.toString());
+
+let redHalfCircle = new TransparentShape(redCircle, 0.5);
+console.log(redHalfCircle.toString());
