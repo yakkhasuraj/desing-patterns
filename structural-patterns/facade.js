595e5e6dcbafeb289aadb61bc3badcf2fc6384c2
Suraj Yakkha <yakkhasuraj@gmail.com>
2024-06-08 21:12:27 +0545

    feat: implement facade pattern

---
+++ structural-patterns/facade.js
@@ -0,0 +1,49 @@
+class Buffer extends Array {
+  constructor(width = 30, height = 20) {
+    super();
+
+    this.width = width;
+    this.height = height;
+    this.alloc(width * height);
+  }
+
+  write(text, position = 0) {
+    //
+  }
+}
+
+class Viewport {
+  constructor(buffer = new Buffer()) {
+    this.buffer = buffer;
+    this.offset = 0;
+  }
+
+  append(text, pos) {
+    this.buffer.write(text, pos + this.offset);
+  }
+
+  getCharAt(index) {
+    return this.buffer[this.offset + index];
+  }
+}
+
+class Console {
+  constructor() {
+    this.buffer = new Buffer();
+    this.currentViewport = new Viewport(this.buffer);
+    this.buffers = [this.buffer];
+    this.viewports = [this.currentViewport];
+  }
+
+  write(text) {
+    this.currentViewport.buffer.write(text);
+  }
+
+  getCharAt(index) {
+    return this.currentViewport.getCharAt(index);
+  }
+}
+
+let c = new Console();
+c.write("hello");
+let ch = c.getCharAt(0);
